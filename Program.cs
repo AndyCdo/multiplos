882639e17b5ef9e7587bb1b1e20ff653c05c198b
Andrea <chela355@hotmail.com>
2021-03-01 16:42:38 -0500

    se hizo la tarea

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Ingrese un numero: ");
+         int numerodeTabla= Convert.ToInt32(Console.ReadLine());
+
+         for (int numero= 1; numero <= 9; numero ++)
+         {
+            int resultado= numerodeTabla * numero;
+            Console.WriteLine(numerodeTabla + "X" + numero + "=" + resultado);
+         }
+         
+          
+
+
+
+
+
+        }
+    }
+}
